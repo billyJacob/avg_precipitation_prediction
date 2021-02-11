c2b0dc221ce16d28d6094a7143aaaeb2f1fce2eb
Billy Jacob <billy.jacob33@gmail.com>
2021-02-11 18:11:16 +0000

    Update avg_precipitation_prediction.py

--- avg_precipitation_prediction.py
+++ avg_precipitation_prediction.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error 
 from sklearn.metrics import mean_absolute_error
-
+import matplotlib.pyplot as plt
 
 dataset = pd.read_csv("ca2-regression.csv")
 
@@ -18,3 +18,15 @@ pd.set_option('precision',3)
 
 description = dataset.describe()
 print(description)
+
+
+correlations = dataset.corr() 
+print(correlations)
+
+skew_data = dataset.skew()
+print(skew_data)
+
+
+histogram   =  dataset.hist()
+
+boxplot = dataset.plot(kind='box',subplots = True , layout=(3,4),sharex = False,sharey =False  )
